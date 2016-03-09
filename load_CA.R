7fcf2ff6b8138234c30ac1f73a067a1bfd399779
Jim Lutz <jdlutz@hotwaterresearch.net>
2016-03-08 18:08:20 -0800

    loaded AllCalEvents, questions on SumAs vs CountAs

--- load_CA.R
+++ load_CA.R
@@ -2,6 +2,7 @@ fn_script = "load_CA.R"
 # first attempt at loading and analyzing Aquacraft data
 
 # Jim Lutz "Mon Mar  7 18:19:03 2016"
+# "Tue Mar  8 14:59:54 2016"    redid ../Aquacraft/*.sh & *.pl to exclude field types from headers
 
 # make sure all packages loaded and start logging
 source("setup.R")
@@ -19,50 +20,95 @@ fn_AllCalEvents <- paste0(wd_Aquacraft,"AllCalEvents_2011.h.csv")
 DT_AllCalEvents <- fread(fn_AllCalEvents)
 # Read 2168699 rows and 9 (of 9) columns from 0.171 GB file in 00:00:11
 
+str(DT_AllCalEvents)
+
+
 names(DT_AllCalEvents)
 # [1] "Keycode"   "SumAs"     "CountAs"   "StartTime" "Duration"  "Peak"      "Volume"    "Mode"      "ModeFreq" 
 
-str(DT_AllCalEvents)
-DT_AllCalEvents[1]
-#   Keycode SumAs CountAs       StartTime Duration  Peak Volume  Mode ModeFreq
-#1:    #INT  TEXT    TEXT SHORT_DATE_TIME     LONG FLOAT  FLOAT FLOAT     LONG
+# change fields to match type in
+# /home/jiml/HotWaterResearch/projects/CECHWT24/hot water calcs/draw patterns/Aquacraft/AllCalEvents_2011.csv.ftype
+# INT,TEXT,TEXT,SHORT_DATE_TIME,LONG,FLOAT,FLOAT,FLOAT,LONG
 
-# convert fields to correct format
+# exam & convert Keycode
+DT_AllCalEvents[, Keycode.i:=as.integer(Keycode)]
+DT_AllCalEvents[,list(unique(Keycode),
+                      is.integer(Keycode.i)),
+                by=Keycode.i][,sort(Keycode.i)]
+DT_AllCalEvents[, Keycode:=Keycode.i]
+DT_AllCalEvents[, Keycode.i:=NULL]
+
+# check SumAs and CountAs
+DT_AllCalEvents[, testAs:= (SumAs==CountAs)]
+DT_AllCalEvents[,length(Keycode),by=testAs]
+#    testAs      V1
+# 1:   TRUE 2132873
+# 2:  FALSE   35825
+
+# check the testAs FALSE
+DT_AllCalEvents[testAs==FALSE,length(Keycode),by=CountAs]
+#          CountAs    V1
+# 1:               34858
+# 2: Clotheswasher    16
+# 3:    Dishwasher     5
+# 4:  Clotheswashe   946
+DT_AllCalEvents[testAs==FALSE,length(Keycode),by=SumAs]
+#             SumAs    V1
+# 1:  Clotheswasher 28704
+# 2:     Dishwasher  7100
+# 3: Clotheswasher@    16
+# 4:    Dishwasher@     5
+DT_AllCalEvents[testAs==FALSE,table(SumAs,CountAs)]
+# CountAs
+# SumAs                  Clotheswashe Clotheswasher Dishwasher
+# Clotheswasher  27758          946             0          0
+# Clotheswasher@     0            0            16          0
+# Dishwasher      7100            0             0          0
+# Dishwasher@        0            0             0          5
+DT_AllCalEvents[,length(Keycode),by=SumAs]
+DT_AllCalEvents[,length(Keycode),by=CountAs]
+
+with(DT_AllCalEvents[SumAs %in% c('Clotheswasher','Dishwasher','','Clotheswasher@','Dishwasher@'),],
+     table(SumAs, CountAs, useNA = "ifany"))
+#               CountAs
+# SumAs                  Clotheswashe Clotheswasher Dishwasher
+#                 7551            0             0          0
+# Clotheswasher  27758          946          7860          0
+# Clotheswasher@     0            0            16          0
+# Dishwasher      7100            0             0       1925
+# Dishwasher@        0            0             0          5
 
 
-# rename some columns
-setnames(DT_RASS_NBr, c("People_.5", "People_6.18", "People_65.99", "People_Total"),
-                      c("N_kids",    "N_youth",     "N_senior",     "N_total"))
+# remove unwanted events
+DT_HotCalEvents <- DT_AllCalEvents[!(SumAs=='Leak'|SumAs=='Toilet'|SumAs=='Irrigation'|SumAs=='Other'),]
 
-# calc number of adults 
-DT_RASS_NBr[,N_adult:=People_19.34 + People_35.54 + People_55.64]
+# change StartTime to a POSIXct date-time object
+DT_HotCalEvents[,start.time := parse_date_time(StartTime,"%a %b %d %H:%M:%S  %Y", tz="America/Los Angeles")]
+# add separate fields for day of week and hour of day
+DT_HotCalEvents[, dow := lubridate::wday(start.time, label=TRUE, abbr=TRUE)]
+DT_HotCalEvents[, hrday := lubridate::hour(start.time)]
 
-# drop the unneeded columns
-DT_RASS_NBr[,c("People_19.34", "People_35.54", "People_55.64"):= NULL]
+# change Duration, Peak, Volume, Mode, ModeFreq to numbers
+DT_HotCalEvents[, Duration := as.numeric(Duration)]
+DT_HotCalEvents[, Peak := as.numeric(Peak)]
+DT_HotCalEvents[, Volume := as.numeric(Volume)]
+DT_HotCalEvents[, Mode := as.numeric(Mode)]
+DT_HotCalEvents[, ModeFreq := as.numeric(ModeFreq)]
 
-# make a code for occupancy combination type
-DT_RASS_NBr[,code:=paste(N_senior,N_adult,N_youth,N_kids,sep="_")]
+str(DT_HotCalEvents)
 
-# reorder columns for easier reading
-setcolorder(DT_RASS_NBr,c("ID", "NBr", "sample_weight", "N_total", "N_senior", "N_adult", "N_youth", "N_kids", "code"))
 
-# Houses with more than 10 people
-DT_RASS_NBr[N_total>10,]
-# 84 houses.
 
-# houses with more than 5 bedrooms
-DT_RASS_NBr[NBr>5,]
-# 181 houses
 
-# houses without any seniors or adults
-DT_RASS_NBr[stri_startswith_fixed(code, "0_0_"),]
-# 818 houses!
 
-DT_RASS_NBr[N_youth>5,]
-DT_RASS_NBr[ID== 25444]
 
-# get rid of undesirable records
-DT_RASS_NBr <- DT_RASS_NBr[N_total<=10 & NBr<=5 & !stri_startswith_fixed(code, "0_0_"),]
+
+
+
+
+
+
+====================
 
 # weight by NBr
 DT_NBr_weight <- DT_RASS_NBr[,list(NBr_weight = sum(sample_weight),NBr_sample=.N), by=NBr][order(NBr),]
